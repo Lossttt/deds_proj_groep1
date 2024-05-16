f1fdd8223699d5f37379674c92ea29d84f8673e9
Lossttt <lfatmi.ayoub@gmail.com>
2024-05-16 19:03:49 +0200

    Updated template

--- 2_Data_Warehouse/utils/database_utils.py
+++ 2_Data_Warehouse/utils/database_utils.py
@@ -1,9 +1,9 @@
 import pyodbc
 import pandas as pd
 
-def create_connection():
+def create_connection(database_name):
     try:
-        DB = {'servername': r'{SERVERNAME}', 'database': r'{DATABASENAME}'}
+        DB = {'servername': r'AYOUB01\SQLEXPRESS', 'database': database_name}
         export_conn = pyodbc.connect('DRIVER={SQL Server};SERVER=' + DB['servername'] + ';DATABASE=' + DB['database'] + ';Trusted_Connection=yes')
         export_cursor = export_conn.cursor()
         return export_conn, export_cursor
@@ -11,8 +11,8 @@ def create_connection():
         print(f"Error occurred while connecting to the database: {e}")
         return None, None
 
-def run_query(query):
-    conn, cursor = create_connection()
+def run_query(query, database_name):
+    conn, cursor = create_connection(database_name)
     try:
         if conn is not None and cursor is not None:
             df = pd.read_sql(query, conn)

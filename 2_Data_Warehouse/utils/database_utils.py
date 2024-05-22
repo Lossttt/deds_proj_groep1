4485a2e80b26aeb4acdf233fbb425cb29757fc18
Lossttt <lfatmi.ayoub@gmail.com>
2024-05-22 18:55:25 +0200

    Updated

--- 2_Data_Warehouse/utils/database_utils.py
+++ 2_Data_Warehouse/utils/database_utils.py
@@ -8,28 +8,53 @@ import os
 dir_path = pathlib.Path(__file__).parent.absolute()
 load_dotenv(dotenv_path=dir_path / '.env')
 
-def create_connectionLocal(database_name):
+def create_connection():
     try:
-        DB = {'servername': r'DESKTOP-VO226NG\SQLEXPRESS01', 'database': database_name}
-        export_conn = pyodbc.connect('DRIVER={SQL Server};SERVER=' + DB['servername'] + ';DATABASE=' + DB['database'] + ';Trusted_Connection=yes')
+        DB = {'servername': os.getenv('DB_SERVERNAME'), 'database': os.getenv('DB_NAME')}
+        export_conn = pyodbc.connect(
+            'DRIVER={SQL Server};SERVER=' + DB['servername'] +
+            ';DATABASE=' + DB['database'] +
+            ';UID=' + os.getenv('DB_USERNAME') +
+            ';PWD=' + os.getenv('DB_PASSWORD')
+        )
         export_cursor = export_conn.cursor()
         return export_conn, export_cursor
     except pyodbc.Error as e:
         print(f"Error occurred while connecting to the database: {e}")
         return None, None
 
-def create_connection():
+def create_local_connection(database_name):
+    """
+    Function to create a connection to the SQL Server database.
+    
+    Parameters:
+    - database_name: Name of the database to connect to.
+    
+    Returns:
+    - conn: Database connection object.
+    - cursor: Database cursor object.
+    """
     try:
-        DB = {'servername': os.getenv('DB_SERVERNAME'), 'database': os.getenv('DB_NAME')}
-        export_conn = pyodbc.connect('DRIVER={SQL Server};SERVER=' + DB['servername'] + ';DATABASE=' + DB['database'] + ';UID=' + os.getenv('DB_USERNAME') + ';PWD=' + os.getenv('DB_PASSWORD'))
-        export_cursor = export_conn.cursor()
-        return export_conn, export_cursor
+        DB = {'servername': r'AYOUB01\SQLEXPRESS', 'database': database_name}
+        conn = pyodbc.connect('DRIVER={SQL Server};SERVER=' + DB['servername'] + ';DATABASE=' + DB['database'] + ';Trusted_Connection=yes')
+        cursor = conn.cursor()
+        return conn, cursor
     except pyodbc.Error as e:
         print(f"Error occurred while connecting to the database: {e}")
         return None, None
 
 def run_query(query, database_name):
-    conn, cursor = create_connectionLocal(database_name)
+    """
+    Function to execute SQL query and return results as DataFrame.
+    
+    Parameters:
+    - query: SQL query to execute.
+    - database_name: Name of the database to execute the query on.
+    
+    Returns:
+    - df: DataFrame containing query results.
+    """
+    conn, cursor = create_connection(database_name)
     if "SELECT" in query.upper():
         df = pd.read_sql(query, conn)
         return df
--- db_utils/db_utils.py
+++ db_utils/db_utils.py
@@ -1,7 +1,29 @@
+import pathlib
 import pyodbc
 import pandas as pd
 
-def create_connection(database_name):
+from dotenv import load_dotenv
+import os
+
+dir_path = pathlib.Path(__file__).parent.absolute()
+load_dotenv(dotenv_path=dir_path / '.env')
+
+def create_connection():
+    try:
+        DB = {'servername': os.getenv('DB_SERVERNAME'), 'database': os.getenv('DB_NAME')}
+        export_conn = pyodbc.connect(
+            'DRIVER={SQL Server};SERVER=' + DB['servername'] +
+            ';DATABASE=' + DB['database'] +
+            ';UID=' + os.getenv('DB_USERNAME') +
+            ';PWD=' + os.getenv('DB_PASSWORD')
+        )
+        export_cursor = export_conn.cursor()
+        return export_conn, export_cursor
+    except pyodbc.Error as e:
+        print(f"Error occurred while connecting to the database: {e}")
+        return None, None
+
+def create_local_connection(database_name):
     """
     Function to create a connection to the SQL Server database.
     
@@ -21,7 +43,7 @@ def create_connection(database_name):
         print(f"Error occurred while connecting to the database: {e}")
         return None, None
 
-def run_query(query, database_name):
+def run_query(query):
     """
     Function to execute SQL query and return results as DataFrame.
     
@@ -32,10 +54,33 @@ def run_query(query, database_name):
     Returns:
     - df: DataFrame containing query results.
     """
-    conn, cursor = create_connection(database_name)
-    if "SELECT" in query.upper():
-        df = pd.read_sql(query, conn)
-        return df
-    else:
-        cursor.execute(query)
-        conn.commit()
+    conn, cursor = create_connection()
+    if conn is None:
+        return None
+    try:
+        if "SELECT" in query.upper():
+            df = pd.read_sql(query, conn)
+            return df
+        else:
+            cursor.execute(query)
+            conn.commit()
+            return None
+    except pyodbc.Error as e:
+        print(f"Error occurred while executing the query: {e}")
+        return None
+
+def run_local_query(query, database_name):
+    conn, cursor = create_local_connection(database_name)
+    if conn is None:
+        return None
+    try:
+        if "SELECT" in query.upper():
+            df = pd.read_sql(query, conn)
+            return df
+        else:
+            cursor.execute(query)
+            conn.commit()
+            return None
+    except pyodbc.Error as e:
+        print(f"Error occurred while executing the query: {e}")
+        return None
